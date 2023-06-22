ea9a6762cdb5025c377ea715f9648dd1a0d720e7
Daniel Alvarez <dalvarez83@users.noreply.github.com>
2023-06-21 23:32:29 -0400

    Add files via upload

---
+++ docs/index.rst
@@ -0,0 +1,24 @@
+.. Microsoft-Planetary-Computer documentation master file, created by
+   sphinx-quickstart.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Microsoft-Planetary-Computer documentation!
+==============================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   getting-started
+   commands
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The Planetary Computer combines a multi-petabyte catalog of global environmental data with intuitive APIs, a flexible scientific environment that allows users to answer global questions about that data, and applications that put those answers in the hands of conservation stakeholders. This repository shows some quick tutorials and guides to get started.',
+    author='Daniel Alvarez',
+    license='BSD-3',
+)
